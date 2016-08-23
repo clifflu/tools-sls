e1b92583e34b9b5c04d44dc6c1701f7082afe08d
Cliff Chao-kuan Lu <clifflu@gmail.com>
2016-08-23 11:17:06 +0800

    feat: ip

---
+++ ip/handler.js
@@ -0,0 +1,41 @@
+'use strict'
+
+function handler (evt, ctx, cb) {
+  try {
+    cb(null, {sourceIp: getIp(evt)})
+  } catch (ex) {
+    cb('unknown sourceIp')
+  }
+}
+
+/**
+ *
+ * @param evt
+ * @param evt.headers
+ * @param evt.identity
+ * @returns IP
+ * @throws Error
+ */
+function getIp (evt) {
+  const candidates = evt.headers['X-Forwarded-For'].split(', ')
+  const probe = evt.identity.sourceIp
+  var idx
+
+  /**
+   * Assumes CF -> API Gateway, so that sourceIp is from CF
+   * Reply with IP before CF for it's the IP customer sees
+   */
+  for (idx = 0; idx < candidates.length; idx++) {
+    if (probe === candidates[idx]) {
+      if (idx === 0) {
+        // Possibly without CF in this deployment
+        return candidates[0]
+      }
+      return candidates[idx - 1]
+    }
+  }
+  throw new Error()
+}
+
+module.exports = handler
+
